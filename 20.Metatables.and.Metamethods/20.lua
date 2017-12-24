85f9c78280c27bd5a761f5ef851272e8ff5d3a6e
luiszhou <luis_zhou@twinstargame.com>
2017-12-24 22:57:45 +0800

    update

---
+++ 20.Metatables.and.Metamethods/20.lua
@@ -0,0 +1,61 @@
+-- predefine the operation.
+
+--Whenever Lua tries to add two tables, it checks whether either of them has a 
+--metatable and whether this metatable has an __add  field. If Lua finds this field, 
+--it calls the corresponding value —the so-called metamethod , which should be a 
+--function to compute the sum.
+
+--Like classes, metatables define the behavior of its instances.
+
+--However, metatables are more restricted than classes,
+--because they can only give behavior to a predefined set of operations; also, metatables do not have in-
+--heritance.
+
+--Tables and userdata have individual metatables; values of other types share one single metatable 
+--for all values of that type.
+
+local t = 1
+print(getmetatable(t))
+
+--From Lua, we can set the metatables only of tables; to manipulate the metatables of values of other types we
+--must use C code or the debug library.
+--bad argument #1 to 'setmetatable' (table expected, got number)
+--t1 = {}
+--setmetatable(t, t1) 
+--print(getmetatable(t) == t1) --> true
+
+print(getmetatable("hi"))
+print(getmetatable("world"))
+assert(getmetatable("hi") == getmetatable("world")) -- oh, this work.
+
+local Set = require("set")
+
+-- Arithmetic Metamethods
+local s1 = Set.new{10, 20, 30, 50}
+local s2 = Set.new{30, 1}
+print(getmetatable(s1))
+print(getmetatable(s2))
+
+local s3 = s1 + s2
+print(Set.tostring(s3)) --> {1, 10, 20, 30, 50}
+
+local s3 = s1 + s2
+print(Set.tostring((s1 + {30, 1})*s1))
+print(Set.tostring(({30, 1} + s1)*s1))
+
+s1 = Set.new{2, 4}
+s2 = Set.new{4, 10, 2}
+print(s1 <= s2)
+--> true
+print(s1 < s2)
+--> true
+print(s1 >= s1)
+--> true
+print(s1 > s1)
+--> false
+print(s1 == s2 * s1) --> true
+
+
+
+
+
---
+++ 20.Metatables.and.Metamethods/set.lua
@@ -0,0 +1,91 @@
+local Set = {}
+local mt = {} -- metatable for sets
+
+-- create a new set with the values of a given list
+function Set.new (l)
+  local set = {}
+  setmetatable(set, mt)
+  for _, v in ipairs(l) do set[v] = true end
+  return set
+end
+function Set.union (a, b)
+--  if getmetatable(a) ~= mt or getmetatable(b) ~= mt then
+--    error("attempt to 'add' a set with a non-set value", 2)
+--  end
+  local res = Set.new{}
+  for k in pairs(a) do res[k] = true end
+  for k in pairs(b) do res[k] = true end
+  return res
+end
+function Set.intersection (a, b)
+  local res = Set.new{}
+  for k in pairs(a) do
+    res[k] = b[k]
+  end
+  return res
+end
+-- presents a set as a string
+function Set.tostring (set)
+  local l = {}
+-- list to put all elements from the set
+  for e in pairs(set) do
+    l[#l + 1] = tostring(e)
+  end
+  return "{" .. table.concat(l, ", ") .. "}"
+end
+
+--subtraction (__sub)
+--float division (__div)
+--floor division (__idiv)
+--negation (__unm)
+--modulo (__mod)
+--exponentiation (__pow)
+
+--bitwise AND (__band), 
+--OR (__bor), 
+--exclusive OR (__bxor), 
+--NOT (__bnot),
+--left shift (__shl), 
+--right shift (__shr)
+
+--concatenation operator __concat
+
+--__eq (equal to)
+--__lt (less than)
+--__le (less than or equal to)
+
+--There are no separate metamethods for the other three relational operators
+--Lua translates a ~= b to not (a == b), a > b to b < a, and a >= b to b <= a
+
+mt.__add = Set.union
+mt.__mul = Set.intersection
+
+--In older versions, Lua translated all order operators to a single one, by translating a <= b to not (b
+--< a). However, this translation is incorrect when we have a partial order, that is, when not all elements
+--in our type are properly ordered. For instance, most machines do not have a total order for floating-point
+--numbers, because of the value Not a Number (NaN). According to the IEEE 754 standard, NaN represents
+--undefined values, such as the result of 0/0. The standard specifies that any comparison that involves NaN
+--should result in false. This means that NaN <= x is always false, but x < NaN is also false. It also
+--means that the translation from a <= b to not (b < a) is not valid in this case.
+
+mt.__le = function (a, b)
+  -- subset
+  for k in pairs(a) do
+    if not b[k] then return false end
+  end
+  return true
+end
+
+mt.__lt = function (a, b)
+  -- proper subset
+  return a <= b and not (b <= a)
+end
+
+mt.__eq = function (a, b)
+--The equality comparison has some restrictions. If two objects have different basic types, the equality
+--operation results in false, without even calling any metamethod. So, a set will always be different from a
+--number, no matter what its metamethod says.
+  return a <= b and b <= a
+end
+
+return Set
